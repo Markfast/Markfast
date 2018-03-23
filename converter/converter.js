387f43f73c33506d184ad363b2ef8660799e35a4
Christopher Thomas <ChristopherCThomas94@gmail.com>
2018-03-23 15:44:25 -0500

    Update

--- converter/converter.js
+++ converter/converter.js
@@ -19,6 +19,16 @@ converter.convertToHTML = function(markdown) {
         htmlElement = htmlElement.replace(regexNewLineSingle, replacer);
     }
 
+    // Header
+    while (regexHeader.test(htmlElement)) {
+        function replacer(match, p1, p2, p3, p4, p5) {
+            let headerIndex = p3.length;
+            return converter.convertToHTML(p1) + "<h" + headerIndex + ">" + p3 + "</h" + headerIndex + ">" + converter.convertToHTML(p5);
+        }
+        htmlElement = htmlElement.replace(regexHeader, replacer);
+        return htmlElement;
+    }
+
     // Bold & Italic
     while (regexBoldAstrickCheck.test(htmlElement)) {
         htmlElement = htmlElement.replace(regexBoldAstrick, "$1<b>$3</b>$5");
