9fd9aeb602c948415b9914446795b35bb1e4694b
Ben Myers <ben.myers@okstate.edu>
2018-03-28 11:03:57 -0500

    English (US) fallback if key is not in a language

--- app/i18n/_i18n.js
+++ app/i18n/_i18n.js
@@ -6,6 +6,7 @@ let langs = require('./_langs.json');
 let config = new Config();
 let currentLangCode = config.get('lang') === undefined ? 'enUS' : config.get('lang');
 let lang = require(`./${getLangSource()}`);
+let enUS = require('./enUS.json');
 
 /**
  * Gets the localization of a string.
@@ -22,6 +23,30 @@ function _(key) {
 
     let tokens = key.split('::');
     let localized = lang;
+    for (let i = 0; i < tokens.length; i++) {
+        localized = localized[tokens[i]];
+        if(localized === undefined) {
+            // If the key is not found in the language, use an English fallback
+            // If all hope is lost, return most specific token
+            return currentLangCode !== 'enUS' ? fallback(key) : tokens[tokens.length - 1];
+        }
+    }
+    if((typeof localized) === 'object') {
+        return localized.label === undefined ? fallback(key) : localized.label;
+    }
+    else {
+        return localized;
+    }
+}
+
+/**
+ * Gets a fallback string in English (US).
+ * @param {string} key - The key of the localized string, in double-colon notation.
+ * @returns The *English (US)* localization of the keyed string, or the most specific identifier of the key if no localization exists.
+ */
+function fallback(key) {
+    let tokens = key.split('::');
+    let localized = enUS;
     for (let i = 0; i < tokens.length; i++) {
         localized = localized[tokens[i]];
         if(localized === undefined) {
