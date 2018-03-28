bdb1358c45824cfb86f66be9488fc03cad3234af
Christopher Thomas <ChristopherCThomas94@gmail.com>
2018-03-28 17:33:31 -0500

    Fixed Link conversion and simplified file

--- converter/converter.js
+++ converter/converter.js
@@ -4,78 +4,31 @@ converter.convertToHTML = function(markdown) {
     let htmlElement = markdown;
 
     // Header
-    /*while (tophetest.test(htmlElement)) {
-        console.log("Here");
-        htmlElement = htmlElement.replace(tophetest, "TEST!!!!");
-        return htmlElement;
-    }*/
-
-    // Header
     while (regexHeader.test(htmlElement)) {
         function replacer(match, p1, p2, p3, p4, p5, p6) {
-            //console.log("1: " + p1, "2: " + p2, "3: " + p3, "4: " + p4, "5: " + p5, "6: " + p6);
             let headerIndex = p3.length;
             return converter.convertToHTML(p1) + "<h" + headerIndex + ">" + p4 + "</h" + headerIndex + ">" + converter.convertToHTML(p5);
         }
-        htmlElement = htmlElement.replace(regexHeader, replacer);
-        return htmlElement;
+        return htmlElement.replace(regexHeader, replacer);
     }
 
     // New Line
-    while(regexNewLineMultiple.test(htmlElement)) {
-        //console.log("Here!");
-        function replacer(match, p1, p2, p3, p4, p5, p6) {
-            // console.log("1: " + p1, "2: " + p2, "3: " + p3, "4: " + p4, "5: " + p5, "6: " + p6);
-            return p1 + "<br />" + p4
-        }
-        htmlElement = htmlElement.replace(regexNewLineMultiple, replacer);
-    }
-    while(regexNewLineSingle.test(htmlElement)) {
-        function replacer(match, p1, p2, p3, p4) {
-            // console.log("1: " + p1, "2: " + p2, "3: " + p3, "4: " + p4);
-            return p1 + p3
-        }
-        htmlElement = htmlElement.replace(regexNewLineSingle, replacer);
-    }
+    while(regexNewLineMultiple.test(htmlElement)) {htmlElement = htmlElement.replace(regexNewLineMultiple, "$1<br />$4");}
+    while(regexNewLineSingle.test(htmlElement)) {htmlElement = htmlElement.replace(regexNewLineSingle, "$1$3");}
 
-    htmlElement = htmlElement.replaceAll("<br />", "\n");
     // Bold & Italic
-    while (regexBoldAsterisk.test(htmlElement)) {
-        function replacer(match, p1, p2, p3, p4, p5, p6, p7) {
-            //console.log("1:", p1, "\n2:", p2, "\n3:", p3, "\n4:", p4, "\n5:", p5, "\n6:", p6, "\n7:", p7);
-            return p1 + "<b>" + p3 + p4 + p5 + "</b>" + p7;
-        }
-        htmlElement = htmlElement.replace(regexBoldAsterisk, replacer);
-    }
-    while (regexBoldUnderscore.test(htmlElement)) {
-        function replacer(match, p1, p2, p3, p4, p5, p6, p7) {
-            //console.log("1:", p1, "\n2:", p2, "\n3:", p3, "\n4:", p4, "\n5:", p5, "\n6:", p6, "\n7:", p7);
-            return p1 + "<b>" + p3 + p4 + p5 + "</b>" + p7;
-        }
-        htmlElement = htmlElement.replace(regexBoldUnderscore, replacer);
-    }
-    console.log(htmlElement);
-    while (regexItalicAsterisk.test(htmlElement)) {
-        function replacer(match, p1, p2, p3, p4, p5, p6, p7, p8) {
-            console.log("1:", p1, "\n2:", p2, "\n3:", p3, "\n4:", p4, "\n5:", p5, "\n6:", p6, "\n7:", p7, "\n8:", p8);
-            return p1 + "<i>" + p3 + p4 + p5 + "</i>" + p7 + p8;
-        }
-        htmlElement = htmlElement.replace(regexItalicAsterisk, replacer);
-    }
-    while (regexItalicUnderscore.test(htmlElement)) {
-        function replacer(match, p1, p2, p3, p4, p5, p6, p7, p8) {
-            console.log(p1, "\n", p2, "\n", p3, "\n", p4, "\n", p5, "\n", p6, "\n", p7, "\n", p8);
-            return p1 + "<i>" + p3 + p4 + p5 + "</i>" + p7 + p8;
-        }
-        htmlElement = htmlElement.replace(regexItalicUnderscore, replacer);
-    }
+    htmlElement = htmlElement.replaceAll("<br />", "\n");
+    while (regexBoldAsterisk.test(htmlElement)) {htmlElement = htmlElement.replace(regexBoldAsterisk, "$1<b>$3$4$5</b>$7");}
+    while (regexBoldUnderscore.test(htmlElement)) {htmlElement = htmlElement.replace(regexBoldUnderscore, "$1<b>$3$4$5</b>$7");}
+    while (regexItalicAsterisk.test(htmlElement)) {htmlElement = htmlElement.replace(regexItalicAsterisk, "$1<i>$3$4$5</i>$7$8");}
+    while (regexItalicUnderscore.test(htmlElement)) {htmlElement = htmlElement.replace(regexItalicUnderscore, "$1<i>$3$4$5</i>$7$8");}
     htmlElement = htmlElement.replaceAll("\n", "<br />");
-    while (regexLink.test(htmlElement)) {
-        htmlElement = htmlElement.replace(regexLink, "$1 <a href=\"$6\">$4</a> $8");
-    }
-    while (regexImage.test(htmlElement)) {
-        htmlElement = htmlElement.replace(regexImage, "$1 <img src=\"$6\" alt=\"$4\"> $8");
-    }
+
+    // Links & Images
+    while (regexLink.test(htmlElement)) {htmlElement = htmlElement.replace(regexLink, "$1$2<a href=\"$6\">$4</a>$8");}
+    while (regexImage.test(htmlElement)) {htmlElement = htmlElement.replace(regexImage, "$1<img src=\"$6\" alt=\"$4\">$8");}
+
+    // Return HTML Element
     return htmlElement
 }
 converter.appendHTMLElement = function (htmlElement) {
