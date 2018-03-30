138f0ee7be1695650ee1f4d2d09434fdeb7cfa95
Christopher Thomas <ChristopherCThomas94@gmail.com>
2018-03-30 12:54:56 -0500

    Updated Regex File

--- converter/regex.js
+++ converter/regex.js
@@ -13,21 +13,11 @@ let regexImage = /([\s\S]*)([\!])(\[)(.*)(\]\()(.*)(\))([\s\S]*)/;
 // Blockquotes
 let regexBlockquote = /(\s*)(\>)(.*)/;
 // Code Blocks
-let regexCode = /([\s\S]*)(\`)([\s\S]*)(\`)([\s\S]*)/;
+let regexCode = /([\s\S]*)(\`)(.*)(\`)([\s\S]*)/;
 let regexCodeBlockBackQuote = /([\s\S]*)(\`\`\`)([\s\S]*)(\`\`\`)([\s\S]*)/;
 let regexCodeBlockTilde = /([\s\S]*)(\~\~\~\~)([\s\S]*)(\~\~\~\~)([\s\S]*)/;
 // Headers
 let regexHeader = /([\s\S]*)(\n\s*)(\#+)(.*)([\s\S]*)/;
 // Br Headers
-//let regexNewLine = /([\s\S]*)(([^\\])(\n)+)([\s\S]*)/;
-
 let regexNewLineSingle = /([\S\s]+)(\n|\r)([\S\s]+)/;
 let regexNewLineMultiple = /([\S\s]+)(\n|\r)(\n+|\r+)([\S\s]+)/;
-
-// bullets
-//let regexUnorderedList = /([\s\S]*)([^\S]+)(\-)([\s\S]*)(\n)([\s\S]*)/
-//let regexUnorderedList = /([\S\s]*)(\n+)(\s+\-)([\S \n]*)((\n(\n*))|(\r\r)*)([\s\S]*)/
-//let regexOrderedList = /([\s\S]*)(\s*)(\d+)(\.)(.*)([\s\S]*)/
-let regexList = /([\S\s]*)(\n\s*)(\-)([\S\s]*)((\n\s*\-)|(\n\n))([\s\S]*)/;
-
-let tophetest = /(a+)([^\*\s])(b+)/;
